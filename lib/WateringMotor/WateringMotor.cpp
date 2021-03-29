c3b8a6c8fd20fab3fd1b69604dda51220ba07d59
t.k.smit <t.k.smit@students.uu.nl>
2021-03-29 16:44:33 +0200

    wateringMotor now non-blocking

--- lib/WateringMotor/WateringMotor.cpp
+++ lib/WateringMotor/WateringMotor.cpp
@@ -1,34 +1,54 @@
 #include <Arduino.h>
 #include "WateringMotor.h"
+#include "../Timer/Timer.h"
+#include "../Stopwatch/Stopwatch.h"
 
-WateringMotor::WateringMotor(int pinNumber) : Servo() {
-    this->pinNumber = pinNumber;
-    this->lastWaterTime = 0;
+WateringMotor::WateringMotor(int pinNumber) : Servo()
+{
+  this->pinNumber = pinNumber;
+  this->lastWaterTime = 0;
 }
 
-void WateringMotor::giveWater(int angle, int duration) {
+void WateringMotor::giveWater(int angle, int duration)
+{
+  Stopwatch durationStopwatch;
+  Timer positionReachTimer;
+  positionReachTimer.start(15);
+
+  this->attach(this->pinNumber);
+  int pos = angle;
+  while (pos >= 0)
+  { // goes from 180 degrees to 0 degrees
+    if (positionReachTimer.hasExpired())
+    {
+      this->write(pos);
+      pos -= 1;
+      positionReachTimer.repeat();
+    }
+  }
 
   this->lastWaterTime = millis();
 
-  this->attach(D6);
-
-  for (int pos = angle; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    this->write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  durationStopwatch.start();
+  unsigned long timeWaiting = 0;
+  while (timeWaiting < duration)
+  {
+    timeWaiting = durationStopwatch.getTime();
   }
-
-  delay(duration); 
-  
-  for (int pos = 0; pos <= angle; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    this->write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  while (pos <= angle)
+  {
+    if (positionReachTimer.hasExpired())
+    {
+      this->write(pos);
+      pos += 1;
+      positionReachTimer.repeat();
+    }
   }
 
   this->detach();
-
 }
 
-unsigned long WateringMotor::getLastWaterTime(){
-    return millis() - this->lastWaterTime;
+unsigned long WateringMotor::getLastWaterTime()
+{
+  return millis() - this->lastWaterTime;
 }
